e4803617cecc4d0d9ea251a6ae5c88848b9dfdf6
Jim Horton <jlhorton72@gmail.com>
2019-03-20 12:27:18 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// console.log("Happy Midterm After Week!");
+const chalk = require('chalk');
+ 
+console.log(chalk.blue('Hello world!'));
+
+var arr = [1, 1, 2, 2, 3, 5, 0]
+ 
+require("uniq")(arr)
+console.log(arr);
